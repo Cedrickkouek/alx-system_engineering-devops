a9c071c82456644957c4e8bf984cc1b88cc55710
cedrickkouek <cedrickkouek@gmail.com>
2024-02-24 10:15:23 +0100

    Moving

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-node '9665f0a47391' {
-  exec { 'install_flask':
-    command => 'pip3 install Flask==2.1.0',
-    path    => '/usr/local/bin:/usr/bin:/bin',
-    unless  => 'pip3 show Flask | grep  2.1.0',
-  }
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
